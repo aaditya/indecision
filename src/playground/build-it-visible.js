02c149a066ae515f9abe3aa6595c5fdadfa687c7
Aaditya Chakravarty <ac36500@gmail.com>
2018-06-07 22:28:04 +0530

    Webpack Integration

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,27 +1,29 @@
-console.log('To-Do Project is running !')
-
-const app = {
-  'title':'Visibility Toggle',
-  'subtitle':'Hey. These are some details you can now see!',
-  'toggle':false
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      'title':'Visibility Toggle',
+      'subtitle':'Hey. These are some details you can now see!',
+      'visible':false
+    }
+  }
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visible: !prevState.visible
+      };
+    });
+  }
+  render() {
+    return(
+      <div>
+        <h1>{this.state.title}</h1>
+          <button onClick={this.handleToggleVisibility}>{this.state.visible?'Hide Details':'Show Details'}</button>
+          {this.state.visible && <p>{this.state.subtitle}</p>}
+      </div>
+    );
+  }
 }
 
-const toggle = () => {
-  app.toggle = !app.toggle;
-  render();
-}
-
-const appRoot = document.getElementById('app');
-
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      <button onClick={toggle}>{app.toggle?'Hide Details':'Show Details'}</button>
-      {app.toggle && <p>{app.subtitle}</p>}
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-}
-
-render();
+ReactDOM.render(<VisibilityToggle />,document.getElementById('app'));
